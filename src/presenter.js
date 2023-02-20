a70d08c5262e8a5d3802aeefde1ae63fa499b445
“Belen <beckiar23@gmail.com>
2023-02-20 16:43:36 -0400

    Precio total con impuesto para todos los estados

--- src/presenter.js
+++ src/presenter.js
@@ -3,17 +3,19 @@ const precio = document.querySelector("#precio_item");
 const estado = document.querySelector("#estado");
 const form = document.querySelector("#totalizador-form")
 const precio_neto_resultado = document.querySelector("#precio-neto-span")
-const estado_resultado = document.querySelector("#estado-span")
 const estado_impuesto_resultado = document.querySelector("#estado-impuesto-span")
+const estado_seleccionado = document.querySelector("#estado-seleccionado")
+const impuesto_resultado = document.querySelector("#impuesto-seleccionado")
+const precio_total_resultado = document.querySelector("#precio-total-span")
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  // const firstNumber = Number.parseInt(first.value);
-
-  precio_neto_resultado.innerHTML = precio.value * items.value;
-  estado_resultado.innerHTML = estado.value;
+  let precioNeto = precio.value * items.value;
+  precio_neto_resultado.innerHTML = precioNeto;
   let impuesto;
+  let precioTotal;
+  let tasaImpuesto;
   switch (estado.value) {
     case "ut":
       impuesto = 6.65; 
@@ -31,5 +33,14 @@ form.addEventListener("submit", (event) => {
       impuesto = 8.25; 
       break;
   }
-  estado_impuesto_resultado.innerHTML = impuesto;
+  tasaImpuesto = (precioNeto * (impuesto / 100));
+  estado_impuesto_resultado.innerHTML = tasaImpuesto;
+  estado_seleccionado.innerHTML = estado.value;
+  impuesto_resultado.innerHTML = impuesto;
+
+  
+  precioTotal = (precioNeto) + tasaImpuesto;
+
+
+  precio_total_resultado.innerHTML = precioTotal;
 });
