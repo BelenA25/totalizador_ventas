902697ccea06558a6b17b195523e40c20920c12e
“Belen <beckiar23@gmail.com>
2023-02-20 11:43:57 -0400

    Ingresar y mostrar cantidad  de items

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,11 @@
-import sumar from "./sumador";
-
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const items = document.querySelector("#cantidad_items");
+const form = document.querySelector("#totalizador-form")
+const div = document.querySelector("#resultado-div")
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  // const firstNumber = Number.parseInt(first.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = items.value;
 });
