7700baf364d420a057b54ecba8fb97d5f82f936d
“Belen <beckiar23@gmail.com>
2023-02-20 12:05:33 -0400

    Ingresar precio y muestra en pantalla

--- src/presenter.js
+++ src/presenter.js
@@ -1,11 +1,14 @@
 const items = document.querySelector("#cantidad_items");
+const precio = document.querySelector("#precio_item");
 const form = document.querySelector("#totalizador-form")
-const div = document.querySelector("#resultado-div")
+const precio_resultado = document.querySelector("#precio-span")
+const items_resultado = document.querySelector("#items-span")
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // const firstNumber = Number.parseInt(first.value);
 
-  div.innerHTML = items.value;
+  items_resultado.innerHTML = items.value;
+  precio_resultado.innerHTML = precio.value;
 });
