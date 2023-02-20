45177ad610493ae022c073c7951810f5266a189a
“Belen <beckiar23@gmail.com>
2023-02-20 12:21:55 -0400

    Selecciona estado y muestra la eleccion

--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,10 @@
 const items = document.querySelector("#cantidad_items");
 const precio = document.querySelector("#precio_item");
+const estado = document.querySelector("#estado");
 const form = document.querySelector("#totalizador-form")
 const precio_resultado = document.querySelector("#precio-span")
 const items_resultado = document.querySelector("#items-span")
+const estado_resultado = document.querySelector("#estado-span")
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -11,4 +13,5 @@ form.addEventListener("submit", (event) => {
 
   items_resultado.innerHTML = items.value;
   precio_resultado.innerHTML = precio.value;
+  estado_resultado.innerHTML = estado.value;
 });
